e07072cee9f60b9ce94b90f6242f5181e2084c20
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2021-12-07 12:01:44 +0100

    Code rewrite

--- tests/test_stream.cpp
+++ tests/test_stream.cpp
@@ -5,62 +5,58 @@
 // and conditions.
 
 #include <stdutils/stdutils.h>
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 #include <fstream>
 #include <string>
 #include <cmath>
 #include <complex>
 
-TEST_CASE("test_stream")
+using namespace Stdutils;
+
+TEST(TestStream, TestFopen)
 {
-    using namespace Stdutils;
+    std::ifstream from;
+    EXPECT_ANY_THROW(fopen(from, "non_existing_file"));
+}
 
-    SECTION("fopen")
-    {
-        std::ifstream from;
-        CHECK_THROWS(fopen(from, "non_existing_file"));
-    }
+TEST(TestStream, TestLexicalCast)
+{
+    std::string s = lexical_cast<std::string>(lexical_cast<double>("  3.14  "));
+    EXPECT_TRUE(s == "3.14");
+}
 
-    SECTION("lexical_cast")
-    {
-        std::string s =
-            lexical_cast<std::string>(lexical_cast<double>("  3.14  "));
-        CHECK(s == "3.14");
-    }
+TEST(TestStream, TestFindToken)
+{
+    std::ifstream from;
+    fopen(from, "test_stream.inp");
 
-    SECTION("find_token")
-    {
-        std::ifstream from;
-        fopen(from, "test_stream.inp");
+    auto pos = find_token(from, "ParseThis");
+    EXPECT_TRUE(pos != -1);
+}
 
-        auto pos = find_token(from, "ParseThis");
-        CHECK(pos != -1);
-    }
+TEST(TestStream, TestGetTokenValue)
+{
+    std::ifstream from;
+    fopen(from, "test_stream.inp");
 
-    SECTION("get_token_value")
-    {
-        std::ifstream from;
-        fopen(from, "test_stream.inp");
+    int i = 0;
+    double d = 0.0;
+    std::complex<double> cx;
+    std::complex<double> cx_ans{2.0, 1.0};
+    std::string s;
 
-        int i = 0;
-        double d = 0.0;
-        std::complex<double> cx;
-        std::complex<double> cx_ans{2.0, 1.0};
-        std::string s;
-
-        auto pos = find_token(from, "ParseThis");
+    auto pos = find_token(from, "ParseThis");
+    if (pos != -1) {
+        get_token_value(from, pos, "integer", i, 100);
+        get_token_value(from, pos, "double", d, 20.0);
+        get_token_value(from, pos, "complex", cx);
+        pos = find_token(from, "SubSection", pos);
         if (pos != -1) {
-            get_token_value(from, pos, "integer", i, 100);
-            get_token_value(from, pos, "double", d, 20.0);
-            get_token_value(from, pos, "complex", cx);
-            pos = find_token(from, "SubSection", pos);
-            if (pos != -1) {
-                get_token_value(from, pos, "string", s, s);
-            }
+            get_token_value(from, pos, "string", s, s);
         }
-        CHECK(i == 100);
-        CHECK(std::abs(d - 2.0) < 1.0e-12);
-        CHECK(cx == cx_ans);
-        CHECK(s == "world");
     }
+    EXPECT_TRUE(i == 100);
+    EXPECT_TRUE(std::abs(d - 2.0) < 1.0e-12);
+    EXPECT_TRUE(cx == cx_ans);
+    EXPECT_TRUE(s == "world");
 }
