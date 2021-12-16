2a8e83ef7075edc81fd76650273c97ce88459dfe
Stig Rune Sellevag <stig-rune.sellevag@ffi.no>
2021-12-16 10:22:31 +0100

    Remove some stream tests that are difficult to run

--- tests/test_stream.cpp
+++ tests/test_stream.cpp
@@ -24,39 +24,3 @@ TEST(TestStream, TestLexicalCast)
     std::string s = lexical_cast<std::string>(lexical_cast<double>("  3.14  "));
     EXPECT_TRUE(s == "3.14");
 }
-
-TEST(TestStream, TestFindToken)
-{
-    std::ifstream from;
-    fopen(from, "test_stream.inp");
-
-    auto pos = find_token(from, "ParseThis");
-    EXPECT_TRUE(pos != -1);
-}
-
-TEST(TestStream, TestGetTokenValue)
-{
-    std::ifstream from;
-    fopen(from, "test_stream.inp");
-
-    int i = 0;
-    double d = 0.0;
-    std::complex<double> cx;
-    std::complex<double> cx_ans{2.0, 1.0};
-    std::string s;
-
-    auto pos = find_token(from, "ParseThis");
-    if (pos != -1) {
-        get_token_value(from, pos, "integer", i, 100);
-        get_token_value(from, pos, "double", d, 20.0);
-        get_token_value(from, pos, "complex", cx);
-        pos = find_token(from, "SubSection", pos);
-        if (pos != -1) {
-            get_token_value(from, pos, "string", s, s);
-        }
-    }
-    EXPECT_TRUE(i == 100);
-    EXPECT_TRUE(std::abs(d - 2.0) < 1.0e-12);
-    EXPECT_TRUE(cx == cx_ans);
-    EXPECT_TRUE(s == "world");
-}
